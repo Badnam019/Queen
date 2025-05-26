315a2349c10d2053ca397482d2f456a2bf0172ec
BADNAM XD <yadavxd746@gmail.com>
2025-05-26 16:14:24 +0530

    Update play.py

--- AnieXEricaMusic/utils/inline/play.py
+++ AnieXEricaMusic/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/ll_ISTKHAR_BABY_lll"),
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/lll_BADNAM_BABY_lll"),
          InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/+9OLH6gomn0IyMjQ1"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -75,7 +75,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/ll_ISTKHAR_BABY_lll"),
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/lll_BADNAM_BABY_lll"),
          InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/+9OLH6gomn0IyMjQ1"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
